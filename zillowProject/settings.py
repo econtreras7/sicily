fe52a165804188df398798478b99cad85bfeec71
Ezequiel Contreras <ezequiel.contreras@flex.com>
2019-03-06 21:37:00 -0800

    Added production host to allowed hosts in settings

--- zillowProject/settings.py
+++ zillowProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('django_sicily')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sicily.herokuapp.com']
 
 
 # Application definition
