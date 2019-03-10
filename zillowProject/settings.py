9b5338a81e3fbd60e5d7916bc0f365b7ffe39724
Ezequiel Contreras <ezequiel.contreras@flex.com>
2019-03-09 23:28:49 -0800

    added postgres

--- zillowProject/settings.py
+++ zillowProject/settings.py
@@ -82,10 +82,14 @@ WSGI_APPLICATION = 'zillowProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': config('postgres_un'),
+        'PASSWORD': config('postgres_un'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
-}
+} 
 
 
 # Password validation
