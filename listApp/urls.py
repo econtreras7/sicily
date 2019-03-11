1810058733523290edce48ca9a19db057df7323b
Ezequiel Contreras <ezequiel.contreras@flex.com>
2019-03-11 09:32:36 -0700

    Adding a property listview

--- listApp/urls.py
+++ listApp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from listApp import views
 from django.contrib import admin
-
+from listApp.views import propertyList
 urlpatterns =[
     path("", views.home, name="home"),
     path("home/",views.home,name ="home"),
     path("hello/<name>", views.hi,name="hi"),
     path("Hello/<name>", views.hi, name="hi"),
+    path("properties/",propertyList.as_view()),
     path("about/", views.about,name="about"),
     path("contact/", views.contact,name="contact")
     
--- listApp/views.py
+++ listApp/views.py
@@ -4,32 +4,38 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.shortcuts import render
 from listApp.models import Property,PropertyImage
+from django.views.generic import ListView
 
 # Create your views here.
 
+
+class propertyList(ListView):
+    model = Property
+
+
+
+
 def home(request):
-    #property = Property.objects.get(pk=1)
-    #image_list = property.images.all()
-    #l = Property.objects.count()
+    property = Property.objects.get(pk=1)
+    image_list = property.images.all()
+    l = Property.objects.count()
     #for(i in l):
-    #allObjects = Property.objects.all()   
+    propertyObjects = Property.objects.all()   
+   # Property.ob
+    #allObjects=1
 
-    allObjects=1
-
-    #print(l)
-
+    print(l)
+    print(propertyObjects[1].title)
 
 
 
-    return render(request,"listApp/home.html",{
-        'propertyObjects:': allObjects
+    return render(request,'listApp/home.html',{
+        'propertyObjects:': propertyObjects
 
-    }
-    
-    
-    
-    
-    )
+    })
+
+
+
     
 def about(request):
     return render(request, "listApp/about.html")
